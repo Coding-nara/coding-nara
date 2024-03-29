bc7c4f00f9412ce7f89c8c2ce47ace237ba7c76c
장영재 <urinaner@naver.com>
2024-03-29 22:40:05 +0900

    comment, board - MVC

---
+++ src/main/java/com/example/codingnara/CodingNaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingnara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingNaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingNaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codingnara/CodingNaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingnara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingNaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
